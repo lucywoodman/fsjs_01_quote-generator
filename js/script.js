d222cc6ffde07f1f824a727dbd93a6d800b53a67
Lucy Woodman <lucyjwoodman@gmail.com>
2021-05-22 13:51:55 +0100

    add quotes array

--- js/script.js
+++ js/script.js
@@ -13,11 +13,35 @@ project 1 - A Random Quote Generator
 
 const quotes = [
   {
-      quote:
-      source:
-      citation:
-      year:
-  }
+      quote: `We are what we repeatedly do. Excellence, then, is not an act, but a habit.`,
+      source: `Will Durant on Aristotle`,
+      citation: `The Story of Philosophy`,
+      year: 1926
+  },
+  {
+    quote: `I am the master of my fate, I am the captain of my soul.`,
+    source: `William Ernest Henley`,
+    citation: `Invictus`,
+    year: 1875
+  },
+  {
+    quote: `A man is but the product of his thoughts. What he thinks, he becomes.`,
+    source: `Mahatma Gandhi`,
+    citation: `Ethical Religion`,
+    year: 1922
+  },
+  {
+    quote: `Be careful how you are talking to yourself, because you are listening.`,
+    source: `Lisa M. Hayes`,
+    citation: `The Power of Self Talk`,
+    year: 2017
+  },
+  {
+    quote: `Life is a matter of choices, and every choice you make makes you.`,
+    source: `John C. Maxwell`,
+    citation: `Beyond Talent: Become Someone Who Gets Extraordinary Results`,
+    year: 2011
+  },
 ];
 
 
